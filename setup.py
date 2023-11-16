bc01c6154d818178bd5a45d8e5e35fc1ae1c41be
Simon Andres <simon.andres@student.uni-tuebingen>
2023-11-16 20:17:22 +0100

    Initial Commit: Folder Structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Building an app to show the most bike-friendly path for a start-destination route',
+    author='Jiazhen Li, Simon Andres',
+    license='',
+)
