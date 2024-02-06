a6bf11c39bae87847b5126885f6d2caa31d7eaed
Jiazhen Li <jiazhen.li@student.uni-tuebingen.de>
2024-02-06 23:19:08 +0100

    computed elevation_gain per edge

--- src/features/elevation.py
+++ src/features/elevation.py
@@ -1,20 +1,18 @@
 import numpy as np
 import pandas as pd
-import geopandas as gpd
 import rasterio as rio
-
+import os
+from pathlib import Path
 import osmnx as ox
-import geopandas as gpd
+from shapely.geometry import LineString
+from scipy.interpolate import griddata
 
-from shapely.geometry import Point, LineString, Polygon, MultiPolygon
-from scipy.spatial import cKDTree
+#################################
+## SET PATHS
+#################################
 
-##############################
-## READING ELEVATION RASTER
-##############################
-
-elv_raster = rio.open('elevation_raster.tif')
-elv_data = elv_raster.read(1)
+BASE_DIR = Path(os.getcwd()).parent
+RASTER_DIR = os.path.join(BASE_DIR, 'data', 'elevation_raster.tif')
 
 ##############################
 ## LOADING OSM DATA
@@ -31,3 +29,45 @@ coords = pd.DataFrame(coords, columns=['geometry'])
 
 # Extract coordinates
 coords['longitude'], coords['latitude'] = zip(*[(point.xy[0][0], point.xy[1][0]) for point in coords.geometry])
+coords['elevation'] = 0
+
+
+
+##############################
+## MAPPING ELEVATION TO OSM
+##############################    
+    
+with rio.open(RASTER_DIR) as dataset:
+    for index, row in coords.iterrows():
+        for val in dataset.sample([(row['longitude'], row['latitude'])]):
+            coords.at[index, 'elevation'] = val[0]
+            
+# -9999 as NaN
+coords['elevation'] = coords['elevation'].apply(lambda x: np.nan if x == -9999 else x)
+
+
+
+##############################
+## INTERPOLATING MISSING ELEVATION
+##############################
+
+# Perform interpolation using griddata from scipy
+coords['elevation_interpolated'] = griddata(coords.dropna(subset=['elevation'])[['longitude', 'latitude']],
+                                            coords.dropna(subset=['elevation'])[['elevation']],
+                                            coords[['longitude', 'latitude']],
+                                            method='linear')
+
+coords['elevation_interpolated'] = interpolated_elevations
+
+# Replace the NaN values in the original elevation column with the interpolated values
+coords['elevation'].fillna(coords['elevation_interpolated'], inplace=True)
+coords.drop(columns=['elevation_interpolated'], inplace=True)
+
+
+
+##############################
+## CALCULATING ELEVATION GAIN PER EDGE
+##############################
+
+# Group by edges    
+coords['elevation_gain'] = coords.groupby('u')['elevation'].transform(lambda x: x.iloc[-1] - x.iloc[0])
