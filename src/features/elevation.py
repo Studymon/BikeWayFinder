5150b5ace50de951d58b5f23cfe1432c91eaf537
Jiazhen Li <jiazhen.li@student.uni-tuebingen.de>
2024-02-13 00:25:28 +0100

    adjusted elevation.py paths; removed unnecessary imputations

--- src/features/elevation.py
+++ src/features/elevation.py
@@ -12,14 +12,16 @@ from scipy.interpolate import griddata
 #################################
 
 BASE_DIR = Path(os.getcwd()).parent.parent
-RASTER_DIR = os.path.join(BASE_DIR, 'src', 'data', 'elevation_raster.tif')
+RASTER_DIR = os.path.join(BASE_DIR, 'data_visible', 'external', 'elevation_raster.tif')
 
 ##############################
 ## LOADING OSM DATA
 ##############################
 
+place_name = 'Stuttgart'
+
 # Extract the graph from OSM
-G = ox.graph_from_place('Stuttgart', network_type='bike')
+G = ox.graph_from_place(place_name, network_type='bike')
 nodes, edges = ox.graph_to_gdfs(G, nodes=True, edges=True)
 
 # Extract coordinates from linestrings of edges
@@ -46,34 +48,7 @@ with rio.open(RASTER_DIR) as dataset:
 coords['elevation'] = coords['elevation'].apply(lambda x: np.nan if x == -9999 else x)
 
 
-
-##############################
-## INTERPOLATING MISSING ELEVATION
-##############################
-
-# Perform interpolation using griddata from scipy
-coords['elevation_interpolated'] = griddata(coords.dropna(subset=['elevation'])[['longitude', 'latitude']],
-                                            coords.dropna(subset=['elevation'])[['elevation']],
-                                            coords[['longitude', 'latitude']],
-                                            method='linear')
-
-# Check if there are still na values after linear interpolation
-if coords['elevation_interpolated'].isna().sum() > 0:
-    # Use 'nearest' method for points that could not be interpolated with 'linear'
-    coords['elevation_interpolated'] = coords.apply(
-        lambda row: row['elevation_interpolated'] if not pd.isna(row['elevation_interpolated']) 
-        else griddata(
-            coords.dropna(subset=['elevation'])[['longitude', 'latitude']],
-            coords.dropna(subset=['elevation'])['elevation'],
-            coords[['longitude', 'latitude']],
-            method='nearest'
-        )[0],
-        axis=1
-    )
-
-# Replace the NaN values in the original elevation column with the interpolated values
-coords['elevation'].fillna(coords['elevation_interpolated'], inplace=True)
-coords.drop(columns=['elevation_interpolated'], inplace=True)
+# No imputation/interpolation needed as NA values are all at the edge of city
 
 
 
@@ -93,5 +68,5 @@ coords = coords[['elevation', 'elevation_gain']]
 ## EXPORTING DATA
 ##############################
 
-OUT_DIR = os.path.join(BASE_DIR, 'src', 'data', 'elevation.csv')
+OUT_DIR = os.path.join(BASE_DIR, 'data_visible', 'processed', 'elevation.csv')
 coords.to_csv(OUT_DIR, index=True)
